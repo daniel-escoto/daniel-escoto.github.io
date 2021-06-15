214d4427eb56fc9ac07f8a567f75aaae7b6b7dd8
Daniel <escotodanny62@gmail.com>
2021-06-14 21:51:11 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function goToSkills() {
+  var element = document.getElementById("skills");
+  element.scrollIntoView({ behavior: "smooth" });
+}
+
+function goToWork() {
+  var element = document.getElementById("work");
+  element.scrollIntoView({ behavior: "smooth" });
+}
+
+function goToContact() {
+  var element = document.getElementById("contact");
+  element.scrollIntoView({ behavior: "smooth" });
+}
